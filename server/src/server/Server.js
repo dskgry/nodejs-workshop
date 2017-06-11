45af782324a3c0470f06418c411d8d3ea62b99b0
dskgry <sven.koelpin@outlook.com>
2017-06-11 17:19:54 +0200

    + 03 exercise branch

--- server/src/server/Server.js
+++ server/src/server/Server.js
@@ -2,16 +2,21 @@
  * @author Sven Koelpin
  */
 
-//TODO
-//- require restify
-//- create server
-//- add get method which returns the json : {message: "hello summit"}
+const restify = require('restify');
 
-const server = null;
+const server = restify.createServer();
+
+server.get('/', (req, res, next) => {
+    res.send({message: 'Hello summit'});
+    next();
+});
+
 
 module.exports = {
     start() {
-        //TODO start the server here (server.listen(port,callback)
+        server.listen(3001, () => {
+            console.log('server up');
+        })
     },
     register(resource){
         resource(server);
