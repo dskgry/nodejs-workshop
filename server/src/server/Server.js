dd05665ed0308c12ccc961548fcdbcb653c2582d
dskgry <sven.koelpin@outlook.com>
2017-06-11 17:18:28 +0200

    + 02 exercise branch

--- server/src/server/Server.js
+++ server/src/server/Server.js
@@ -7,8 +7,16 @@
 //- create server
 //- add get method which returns the json : {message: "hello summit"}
 
+const server = null;
+
 module.exports = {
     start() {
         //TODO start the server here (server.listen(port,callback)
+    },
+    register(resource){
+        resource(server);
+    },
+    getServer(){
+        return server;
     }
 };
