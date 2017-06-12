8877ddaf28afe0e1ddb0884157d1950fe2adcfc7
Sven Koelpin <sven@OLOK-PO-129.local>
2017-06-12 12:35:24 +0200

    adjust exercise 05

--- server/src/tweets/TweetsResource.js
+++ server/src/tweets/TweetsResource.js
@@ -2,15 +2,19 @@
  * @author Sven Koelpin
  */
 
+//TODO
+//- require Validation middleware
+//- require yup
+
 const tweetService = require('./TweetService');
 
 module.exports = server => {
     //TODO
-    // - Add validation (use the Validation-middleware, location in src/server/common/Validation)
     // - Add validation (validateQueryParams)
     //   - page (number, min 1, max 10, default 1
     //   - size (number, min 1, max 100, default 10
     server.get('tweets', (req, res, next) => {
+        //TODO parsing + default param handlung can be removed when validation is added
         const page = req.params.page ? parseInt(req.params.page, 10) : 1;
         const size = req.params.size ? parseInt(req.params.size, 10) : 10;
         const start = (page - 1) * size;
@@ -21,6 +25,7 @@ module.exports = server => {
     //TODO:
     // - Implement createTweet, which listens to HTTP-POST (path: 'tweets'). Use  TweetService#createTweet
     // - The posted tweet is saved in req.body (don't forget to add the bodyParser()-middleware in Server.js)
+    // - respond with the created tweet and the status code 201
     // - Add validation (validatePostBody).
     //  - a tweet needs to have at least the properties:
     //      - tweet (string, min. 3 chars, max 100 chars, required)
@@ -30,6 +35,7 @@ module.exports = server => {
     // - Implement getTweet which gets a single tweet by its id.
     //   - The method is a HTTP-GET method that has a dynamic path parameter :id (path: 'tweets/:id).
     //   - Use TweetService#getTweet
-    //   - The path-parameter can be found in req.params
+    //   - The path-parameter can be found in req.params (don't forget to parse it to a number!)
+    //   - Return status code 404 when there is no tweet for the given id
 
 };
