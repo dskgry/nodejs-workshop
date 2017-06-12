bbea5e79a426d114441c64490a1184535a57b086
Sven Koelpin <sven@OLOK-PO-129.local>
2017-06-12 10:28:08 +0200

    + adjusted tweetservice exercise

--- server/src/tweets/TweetService.js
+++ server/src/tweets/TweetService.js
@@ -8,8 +8,8 @@ const fakeDataBase = require('../db/FakeDatabase');
 
 const getTweets = async (start, size) => {
     //TODO: Return all tweets (use the fakeDataBase)
-    //- sorted by id (Tip: use the array.sort-function, e.g. [1,2,3].sort((a,b)=>a<b))
-    //- use the given start + size parameters to create a sub-array (Tip: use the array.slice-function)
+    //- sorted by id (Hint: use the array.sort-function, e.g. [1,2,3].sort((a,b)=>a<b))
+    //- use the given start + size parameters to create a sub-array (Hint: use the array.slice-function)
     return [];
 };
 
@@ -17,7 +17,7 @@ const getTweet = async id => {
     const tweetId = parseInt(id, 10);
 
     //TODO return a single tweet by it's tweetId
-    //- Tip: use the array.find-function
+    //- Hint: use the array.find-function
     return null;
 };
 
@@ -28,7 +28,7 @@ const countTweets = async () => {
 
 const createTweet = async tweet => {
     //TODO
-    //- add the tweet to the database (TIP: use array.push)
+    //- add the tweet to the database (Hint: use array.push)
     //- set the id of a tweet BEFORE adding it
     //- return the created tweet
     return null;
--- server/src/tweets/TweetsResource.js
+++ server/src/tweets/TweetsResource.js
@@ -4,10 +4,11 @@
 
 module.exports = server => {
     server.get('tweets', (req, res, next) => {
-        const {page, size} = req.params; //read query params
+        const page = 0; //TODO read from query parameters. Hint: req.params
+        const size = 0; //TODO read from query parameters  Hint: req.params
         const start = (page - 1) * size;
 
-        const allTweets = []; //TODO use tweetservice, don't forget the start + size params
+        const allTweets = []; //TODO use tweetservice, pass start + size as params
         res.send(allTweets);
         next();
     });
