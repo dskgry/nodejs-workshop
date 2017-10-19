5b6544b0feb347bcac5c369609547f024bf45366
dskgry <sven.koelpin@outlook.com>
2017-10-19 17:59:27 +0200

    - changed hints

--- server/src/server/Server.js
+++ server/src/server/Server.js
@@ -5,18 +5,21 @@
 //TODO
 //- require restify
 //- create server
-//- add get method which returns the json : {message: "hello summit"}
+//- add get method to the server which uses the path "/" and returns the json : {message: "hello IJS"}
 
 const server = null;
 
 module.exports = {
     start() {
-        //TODO start the server here (server.listen(port,callback)
+        //TODO
+        //start the server here (server.listen(port,callback))
+        //port should be 3001
+        //server will be listening at http://localhost:3001 when started.
     },
-    register(resource){
+    register(resource) {
         resource(server);
     },
-    getServer(){
+    getServer() {
         return server;
     }
 };
