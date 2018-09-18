620f18f3548262b4fa569ed5d490444d0fb5a5fa
sven <sven.koelpin@openknowledge.de>
2018-09-18 13:41:00 +0200

    ex_2 update

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,4 @@
 require('dotenv').load();
-const dataBase = require('./db/Database');
-const logger = require('./server/Logger');
 const server = require('./server/Server');
 
-const tweetsResource = require('./tweets/TweetsResource');
-
-server.register(tweetsResource);
-
-dataBase.init()
-    .then(() => server.start())
-    .catch(() => {
-        logger.warn('DB not running. Using in memory data');
-        server.start();
-    });
+server.start();
