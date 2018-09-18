d3b2c311a1ec8236012ac55f0b3c159e29e1ef1a
sven <sven.koelpin@openknowledge.de>
2018-09-18 14:24:56 +0200

    ex_07 update

--- server/src/server/Server.js
+++ server/src/server/Server.js
@@ -1,9 +1,12 @@
 /**
  * @author Sven Koelpin
  */
-//TODO require logger + security
 const restify = require('restify');
+const webSocket = require('ws');
+const logger = require('./Logger');
+const security = require('../security/Security');
 const corsMiddleware = require('restify-cors-middleware');
+const eventEmitter = require('./Events');
 
 const {
     ALLOWED_ORIGINS,
@@ -20,14 +23,22 @@ const cors = corsMiddleware({
 const server = restify.createServer();
 
 //middlewares pre
-//TODO add logger middleware
+server.pre(logger);
 server.pre(cors.preflight);
 //middlewares use plugins
 server.use(cors.actual);
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 
-//TODO add security middleware
+server.use(security);
+
+//socket
+const wss = new webSocket.Server({server});
+//TODO
+// - listen to 'connection' - event of wss
+// - listen to eventEmitters 'newData' event (eventEmitters.addListener('event',handler))
+// - push the data to the client when the newData-Event is fired (ws.send(JSON.stringify(data))) <-- JSON.stringify is important here ;)
+// - remove the listener (eventEmitter.removeListener('event', handler)) when a client disconnects ('close'-event)
 
 module.exports = {
     register(resource) {
